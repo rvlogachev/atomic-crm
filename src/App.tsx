63b5cf954268732f9494472fc167206dcf3cb820
Roman <rvlogachev@gmail.com>
2025-11-16 16:29:39 +0300

    переименовал header

--- src/App.tsx
+++ src/App.tsx
@@ -30,21 +30,32 @@ import { MailCheck, MailCheckIcon } from "lucide-react";
 const App = () => (
     <CRM 
         title='SM CRM'
-        companySectors={['Technology', 'Finance']}
-        dealCategories={['Copywriting', 'Design']}
+        companySectors={['Металлургия', 'Нефтегаз и химия', 'Логистика и транспорт', 'Склады и e-commerce', 'Ритейл и FMCG', 'Ритейл и FMCG', 'Машиностроение', 'Финансы и страхование', 'Стройка', 'IT и телекоммуникации', 'Образование', 'Медицина и фармацевтика', 'Госсектор', 'Другое']}
+        dealCategories={['Имитационная модель', 'Пилот / PoC', 'Цифровой двойник (платформа)', 'Оптимизационный модуль', 'AI-ассистент / чат-бот', 'Дашборды', 'Обучение и тренинги', 'Консалтинг', 'Лицензии и ПО партнёров','Другое']}
         dealPipelineStatuses={['won']}
         dealStages={[
-            { value: 'opportunity', label: 'Opportunity' },
-            { value: 'proposal-sent', label: 'Proposal Sent' },
-            { value: 'won', label: 'Won' },
-            { value: 'lost', label: 'Lost' },
+            { value: 'opportunity', label: 'Новый лид' },
+            { value: 'first-contact', label: 'Первичный контакт' },
+            { value: 'qualification', label: 'Квалификация|сбор требований' },
+            { value: 'prepare', label: 'Подготовка предложения' },
+            { value: 'proposal-sent', label: 'КП отправлено' },
+            { value: 'agree-order', label: 'Переговоры и согласование' },
+            { value: 'contract', label: 'Договор / тендер' },
+            { value: 'contract', label: 'Проект в работе' },
+            { value: 'won', label: 'Закрыта: успешно' },
+            { value: 'lost', label: 'Закрыта: неуспешно' },
         ]}
         noteStatuses={[
-            { value: 'cold', label: 'Cold', color: '#7dbde8' },
-            { value: 'warm', label: 'Warm', color: '#e8cb7d' },
-            { value: 'hot', label: 'Hot', color: '#e88b7d' },
+            { value: 'cold', label: 'Лид', color: '#7dbde8' },
+            { value: 'cold', label: 'Потенциальный клиент', color: '#7dd8e8ff' },
+            { value: 'cold', label: 'Клиент', color: '#7de8d8ff' },
+            { value: 'cold', label: 'Ключевой клиент', color: '#7de8b6ff' },
+            { value: 'cold', label: 'Партнёр / интегратор', color: '#96e87dff' },
+            { value: 'cold', label: 'Подрядчик / эксперт', color: '#d1e87dff' },
+            { value: 'warm', label: 'Тёплый', color: '#e8cb7d' },
+            { value: 'hot', label: 'Горячий', color: '#e88b7d' },
         ]}
-        taskTypes={['Call', 'Email', 'Meeting']}
+        taskTypes={['Звонок', 'Email', 'Онлайн-встреча', 'Очная встреча', 'Демонстрация продукта', 'Подготовка КП', 'Документы и договоры', 'Внутреннее обсуждение', 'Обучение клиента', 'Другое']}
     disableTelemetry />
 );
 
